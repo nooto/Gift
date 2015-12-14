4d1cb660b6de1cb277e367c517464cdab0c54f00
GaoAng <gaoang@GaoAngdeiMac.local>
2015-12-14 20:55:52 +0800

    补充代码

--- Gift/Gift/UI/WeChat/GWCViewController.m
+++ Gift/Gift/UI/WeChat/GWCViewController.m
@@ -12,6 +12,10 @@
 @property (nonatomic, strong) UIImageView  *mAvatorImage;
 @property (nonatomic, strong) UITextField  *mNameTextField;
 
+
+@property (nonatomic, strong) UIButton *mSendButton;
+@property (nonatomic, strong) UIButton *mReceiveButton;
+@property (nonatomic, strong) UIButton *mHistoryButton;
 @end
 @implementation GWCViewController
 
@@ -19,6 +23,10 @@
     [super viewDidLoad];
     [self.view addSubview:self.mAvatorImage];
     [self.view addSubview:self.mNameTextField];
+    
+    [self.view addSubview:self.mSendButton];
+    [self.view addSubview:self.mReceiveButton];
+    [self.view addSubview:self.mHistoryButton];
     
     __weak typeof(self.view )weakSelfView = self.view;
     [self.mAvatorImage mas_makeConstraints:^(MASConstraintMaker *make) {
@@ -34,7 +42,31 @@
         make.width.equalTo(weakSelfView.mas_width).multipliedBy(0.5);
         make.height.mas_equalTo(30);
     }];
+    
+    [self.mSendButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(weakSelfView.mas_left);
+        make.top.equalTo(weakSelfView.mas_bottom).offset(-45);
+        make.bottom.equalTo(weakSelfView.mas_bottom);
+        make.width.equalTo(self.mReceiveButton.mas_width);
+    }];
+    
+    [self.mReceiveButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.mSendButton.mas_right);
+        make.width.equalTo(self.mHistoryButton.mas_width);
+        make.top.equalTo(self.mSendButton.mas_top);
+        make.bottom.equalTo(self.mHistoryButton.mas_bottom);
+    }];
+    
+    [self.mHistoryButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.mReceiveButton.mas_right);
+        make.right.equalTo(weakSelfView.mas_right);
+        make.top.equalTo(self.mSendButton.mas_top);
+        make.bottom.equalTo(self.mSendButton.mas_bottom);
+        make.width.equalTo(self.mSendButton.mas_width);
+        make.height.equalTo(self.mSendButton.mas_height);
+    }];
 }
+
 
 -(void)viewDidLayoutSubviews{
     [super viewDidLayoutSubviews];
@@ -59,8 +91,53 @@
         _mNameTextField.layer.cornerRadius = 8.0f;
         _mNameTextField.layer.borderColor = Color_black_30.CGColor;
         _mNameTextField.layer.borderWidth = 0.5f;
+        _mNameTextField.clearButtonMode = UITextFieldViewModeAlways;
+        
+        _mNameTextField.leftView.backgroundColor = [UIColor greenColor];
+        _mNameTextField.leftViewMode = UITextFieldViewModeAlways;
+        
+        _mNameTextField.rightView.backgroundColor = [UIColor redColor];
+        _mNameTextField.rightViewMode = UITextFieldViewModeAlways;
     }
     return _mNameTextField;
 }
+
+-(UIButton*)mSendButton{
+    if (!_mSendButton) {
+        _mSendButton = [UIButton new];
+        [_mSendButton setTitle:@"收到" forState:UIControlStateNormal];
+        _mSendButton.layer.borderWidth = 1.0f;
+        _mSendButton.layer.borderColor = Color_black_50.CGColor;
+        _mSendButton.showsTouchWhenHighlighted = YES;
+        [_mSendButton setTitleColor:Color_black_50 forState:UIControlStateNormal];
+    }
+    return _mSendButton;
+}
+
+-(UIButton*)mReceiveButton{
+    if (!_mReceiveButton) {
+        _mReceiveButton = [UIButton new];
+        [_mReceiveButton setTitle:@"发出" forState:UIControlStateNormal];
+        _mReceiveButton.layer.borderWidth = 1.0f;
+        _mReceiveButton.layer.borderColor = Color_black_50.CGColor;
+        _mReceiveButton.showsTouchWhenHighlighted = YES;
+        [_mReceiveButton setTitleColor:Color_black_50 forState:UIControlStateNormal];
+    }
+    return _mReceiveButton;
+}
+
+-(UIButton*)mHistoryButton{
+    if (!_mHistoryButton) {
+        _mHistoryButton = [UIButton new];
+        [_mHistoryButton setTitle:@"历史" forState:UIControlStateNormal];
+        _mHistoryButton.layer.borderWidth = 1.0f;
+        _mHistoryButton.layer.borderColor = Color_black_50.CGColor;
+        _mHistoryButton.showsTouchWhenHighlighted = YES;
+        [_mHistoryButton setTitleColor:Color_black_50 forState:UIControlStateNormal];
+    }
+    return _mHistoryButton;
+}
+
+
 
 @end
