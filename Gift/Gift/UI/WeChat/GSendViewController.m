f1b51396108f2c38f87bc4e9424f11d960ff7034
GaoAng <gaoang@GaoAngdeiMac.local>
2015-12-26 17:45:00 +0800

    功能代码补充

--- Gift/Gift/UI/WeChat/GSendViewController.m
+++ Gift/Gift/UI/WeChat/GSendViewController.m
@@ -151,25 +151,123 @@
     lineView.backgroundColor = Color_black_30;
     [bottomView addSubview:lineView];
     
-    [lineView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.height.mas_equalTo(0.5);
-        make.left.equalTo(bottomView.mas_left).offset(20);
-        make.right.equalTo(bottomView.mas_right);
-        make.centerY.mas_equalTo(bottomView.mas_centerY);
-    }];
-//    self.
+//    [lineView mas_makeConstraints:^(MASConstraintMaker *make) {
+//        
+//        make.width.equalTo(bottomView.mas_width);
+//        make.height.mas_equalTo(0.5f);
+//        make.centerX.equalTo(bottomView.mas_height).dividedBy(2);
+////        make.bottom.equalTo(weakSelf.view.mas_bottom);
+//
+////        make.left.equalTo(bottomView.mas_left).offset(20);
+////        make.right.equalTo(bottomView.mas_right);
+////        make.centerY.equalTo(bottomView.mas_height).multipliedBy(0.5);
+////        make.height.mas_equalTo(0.5);
+////        make.width.equalTo(bottomView.mas_width).offset(-20);
+////        make.centerY.mas_equalTo(bottomView.mas_centerY);
+//    }];
+////    self.
+    
+    UILabel *pingLabel = [UILabel new];
+    [pingLabel setText:@"拼"];
+    pingLabel.textColor = [UIColor whiteColor];
+    pingLabel.backgroundColor = Color_orange_F866822;
+    pingLabel.font = Font15;
+    pingLabel.layer.cornerRadius = 2.0f;
+    [self.view addSubview:pingLabel];
     
     [self.view addSubview:self.mFirstCount];
+    [self.view addSubview:pingLabel];
     [self.view addSubview:self.mFirstName];
+    
+    
     [self.view addSubview:self.mSecondCount];
+    
+    UILabel *pingLabel2 = [UILabel new];
+    [pingLabel2 setText:pingLabel.text];
+    pingLabel2.textColor = pingLabel.textColor;
+    pingLabel2.backgroundColor = pingLabel.backgroundColor;
+    pingLabel2.font = pingLabel.font;
+    pingLabel2.layer.cornerRadius = pingLabel.layer.cornerRadius;
+    [self.view addSubview:pingLabel2];
+
+    
     [self.view addSubview:self.mSecondName];
     
-    [self.mSecondName mas_makeConstraints:^(MASConstraintMaker *make) {
+    [self.mFirstName mas_makeConstraints:^(MASConstraintMaker *make) {
         make.left.equalTo(weakSelf.view.mas_left).offset(-20);
-        make.width.equalTo(weakSelf.view.mas_width).dividedBy(2).offset(20);
-        make.bottom.equalTo(bottomView.mas_bottom).offset(20);
-        
+        make.centerY.equalTo(bottomView.mas_height).dividedBy(4);
+        make.width.mas_equalTo(40);
+        make.height.mas_equalTo(25);
+    
     }];
+//    self.mFirstName.mas_
+    
+    [pingLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerY.equalTo(weakSelf.mFirstName.mas_centerY);
+        make.left.equalTo(weakSelf.mFirstName.mas_right);
+        make.width.mas_equalTo(30);
+        make.width.mas_equalTo(30);
+    }];
+    
+    UILabel *firstDate = [UILabel new];
+    [firstDate setFont:Font13];
+    [firstDate setTextColor:Color_black_30];
+    NSDate *date = [NSDate date];
+    [firstDate setText:[NSString stringWithFormat:@"%ld-%2ld",(long)date.month,(long)date.day]];
+    [bottomView addSubview:firstDate];
+    
+    [firstDate mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(weakSelf.mFirstName.mas_left);
+        make.top.equalTo(weakSelf.mFirstName.mas_bottom);
+        make.width.equalTo(weakSelf.mFirstName.mas_width);
+        make.height.equalTo(weakSelf.mFirstName.mas_height);
+    }];
+    
+    [self.mFirstCount mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.right.equalTo(weakSelf.view.mas_right).offset(-20);
+        make.centerY.equalTo(weakSelf.mFirstName.mas_centerY);
+        make.height.equalTo(weakSelf.mFirstName.mas_height);
+        make.width.equalTo(weakSelf.mFirstName.mas_width);
+    }];;
+    
+    
+    
+    [self.mSecondName mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(weakSelf.mFirstName.mas_left);
+        make.centerY.equalTo(bottomView.mas_height).multipliedBy(0.75);
+        make.width.mas_equalTo(weakSelf.mFirstName.width);
+        make.height.mas_equalTo(weakSelf.mFirstName.height);
+    }];
+    
+    [pingLabel2 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerY.equalTo(weakSelf.mSecondName.mas_centerY);
+        make.left.equalTo(weakSelf.mSecondName.mas_right);
+        make.width.mas_equalTo(30);
+        make.width.mas_equalTo(30);
+    }];
+    
+    [self.mSecondCount mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.right.equalTo(weakSelf.view.mas_right).offset(-20);
+        make.centerY.equalTo(weakSelf.mSecondName.mas_centerY);
+        make.height.equalTo(weakSelf.mSecondName.mas_height);
+        make.width.equalTo(weakSelf.mSecondName.mas_width);
+    }];;
+    
+    UILabel *secondDate = [UILabel new];
+    [secondDate setFont:Font13];
+    [secondDate setTextColor:Color_black_30];
+    NSDate *date22 = [NSDate date];
+    [secondDate setText:[NSString stringWithFormat:@"%ld-%2ld",(long)date22.month,(long)date22.day]];
+    [bottomView addSubview:secondDate];
+    
+    [secondDate mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(weakSelf.mSecondName.mas_left);
+        make.top.equalTo(weakSelf.mSecondName.mas_bottom);
+        make.width.equalTo(weakSelf.mSecondName.mas_width);
+        make.height.equalTo(weakSelf.mSecondName.mas_height);
+    }];
+
+    
     
     
     
@@ -196,24 +294,109 @@
     }
 }
 
+#pragma mark -=====自定义截屏位置大小的逻辑代码=====-
+//static int ScreenshotIndex=0; //这里的逻辑直接采用上面博主的逻辑了
+-(UIImage*)ScreenShot{
+//    for (UIWindow *subWindow in [UIApplication sharedApplication].windows) {
+//        
+//        NSLog(@"%d",subWindow.windowLevel);
+//    }
+    
+    CALayer *layer = [[UIApplication sharedApplication] keyWindow].layer;
+    CGFloat scale = [UIScreen mainScreen].scale;
+    UIGraphicsBeginImageContextWithOptions(layer.frame.size, NO, scale);
+    
+    [layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *img = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return img;
+//    
+//    CGSize imageSize = CGSizeMake(SCREEN_W, SCREEN_H);
+//    NSLog(@"%f,%f",imageSize.width,imageSize.height);
+//    
+//    //支持retina就靠这句话，不带参数的begin保存的图片分辨率很低的。
+//    
+////    if (UIGraphicsBeginImageContextWithOptions != NULL) {
+//    
+//        UIGraphicsBeginImageContextWithOptions(imageSize, NO, 0.0);
+//        
+////    } else {
+////        
+////        UIGraphicsBeginImageContext(imageSize);
+////        
+////    }
+    
+//    
+//    [self.view.layer renderInContext:UIGraphicsGetCurrentContext()];
+//    
+//    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+//    
+//    UIGraphicsEndImageContext();
+//    return image;
+//    
+    
+//    //这里因为我需要全屏接图所以直接改了，宏定义iPadWithd为1024，iPadHeight为768，
+//    //    UIGraphicsBeginImageContextWithOptions(CGSizeMake(640, 960), YES, 0);     //设置截屏大小
+//    UIGraphicsBeginImageContextWithOptions(CGSizeMake(SCREEN_W, SCREEN_H), YES, 0);     //设置截屏大小
+//    [[self.navigationController.view layer] renderInContext:UIGraphicsGetCurrentContext()];
+//    UIImage *viewImage = UIGraphicsGetImageFromCurrentImageContext();
+//    UIGraphicsEndImageContext();
+//    CGImageRef imageRef = viewImage.CGImage;
+//    //    CGRect rect = CGRectMake(166, 211, 426, 320);//这里可以设置想要截图的区域
+//    CGRect rect = CGRectMake(0, 0, SCREEN_W, SCREEN_H);//这里可以设置想要截图的区域
+//    CGImageRef imageRefRect =CGImageCreateWithImageInRect(imageRef, rect);
+//    UIImage *sendImage = [[UIImage alloc] initWithCGImage:imageRefRect];
+//    UIImageWriteToSavedPhotosAlbum(sendImage, nil, nil, nil);//保存图片到照片库
+////    NSData *imageViewData = UIImagePNGRepresentation(sendImage);
+////    
+////    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+////    NSString *documentsDirectory = [paths objectAtIndex:0];
+////    NSString *pictureName= [NSString stringWithFormat:@"screenShow_%d.png",ScreenshotIndex];
+////    NSString *savedImagePath = [documentsDirectory stringByAppendingPathComponent:pictureName];
+////    NSLog(@"截屏路径打印: %@", savedImagePath);
+////    //这里我将路径设置为一个全局String，这里做的不好，我自己是为了用而已，希望大家别这么写
+////    [self SetPickPath:savedImagePath];
+////    
+////    [imageViewData writeToFile:savedImagePath atomically:YES];//保存照片到沙盒目录
+//    CGImageRelease(imageRefRect);
+////    ScreenshotIndex++;
+//    return sendImage;
+}
+
+////设置路径
+//- (void)SetPickPath:(NSString *)PickImage {
+//    _ScreenshotsPickPath = PickImage;
+//}
+////获取路径<这里我就直接用于邮件推送的代码中去了，能达到效果，但肯定有更好的写法>
+//- (NSString *)GetPickPath {
+//    return _ScreenshotsPickPath;
+//}
+//
+
 
 -(void)showSaveTIp{
     
-    UIImageView *imagev = [[UIImageView alloc] init];
-    imagev.image = [UIImage imageWithView:self.view];
     
-    WeakSelf(weakSelf);
-    EHCustomAlertView *alertView = [[EHCustomAlertView alloc] initWithTitle:nil message:@"是否保存到相册" leftButton:@"不保存" rightButton:@"保存" selectActin:^(NSInteger index) {
-        if (index == 1) {
-            weakSelf.isSave = YES;
-            UIImageWriteToSavedPhotosAlbum(imagev.image, self, @selector(image:didFinishSavingWithError:contextInfo:), NULL);
-        }
-        else{
-            weakSelf.isSave = NO;
-        }
-    }];
-    
-    [alertView show];
+
+//    UIImageView *imagev = [[UIImageView alloc] init];
+//    imagev.image = [UIImage imageWithView:[UIApplication sharedApplication].keyWindow];
+//    imagev.image = [UIImage imageWithView:[UINavigationBar appearance]];
+////    imagev.image = [self ScreenShot];
+////    UIView *screenshotView = [[self view] snapshotViewAfterScreenUpdates:YES];
+////    imagev.image = [UIImage imageWithView:screenshotView];
+//    
+//    WeakSelf(weakSelf);
+//    EHCustomAlertView *alertView = [[EHCustomAlertView alloc] initWithTitle:nil message:@"是否保存到相册" leftButton:@"不保存" rightButton:@"保存" selectActin:^(NSInteger index) {
+//        if (index == 1) {
+//            weakSelf.isSave = YES;
+//            UIImageWriteToSavedPhotosAlbum(imagev.image, self, @selector(image:didFinishSavingWithError:contextInfo:), NULL);
+//        }
+//        else{
+//            weakSelf.isSave = NO;
+//        }
+//    }];
+//    
+//    [alertView show];
 }
 
 - (void)image: (UIImage *) image didFinishSavingWithError: (NSError *) error contextInfo: (void *) contextInfo{
